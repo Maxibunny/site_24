eb223ef419016a0596aa4539f66909e0a4052b5a
yssv-vw <118190708+yssv-vw@users.noreply.github.com>
2024-05-31 02:06:15 +0300

    article_test.rb

--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -1,7 +1,20 @@
 require "test_helper"
 
 class ArticleTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "Can't have articles with the same title" do
+    art1 = Article.create(title: "New title", body: "123456789 987654321")
+    art2 = Article.new(title: "New title", body: "123456789 987654321")
+    assert_not art2.save, "Can't have articles with the same title"
+  end
+
+  test "Can't create articles without title" do
+    art = Article.new(body: "123456789 987654321")
+    assert_not art.save, "Cant't have articles without title"
+  end
+
+  test "Can't create articles with empty body" do
+    art = Article.new(title: "New title")
+    assert_not art.save, "Can't create articles with empty body"
+  end
 end
