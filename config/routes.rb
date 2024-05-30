93ded4c4d4c6781c9243b75085b9fe81c257c1c1
Maxibunny <max2003rnd@gmail.com>
2024-05-30 20:48:45 +0300

    authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,30 @@
 Rails.application.routes.draw do
-
-  root "articles#index"
+  get 'sessions/new'
+  post 'sessions/create'
+  #get 'sessions/create'
+  delete 'sessions/destroy'
+  #get 'sessions/destroy'
+  get 'users/new'
+  post 'users/create'
+  #get 'users/create'
+  get 'pages/exclusive'
 
   resources :articles do
     resources :comments
   end
 
+
+  get 'signup', to: 'users#new'
+  post 'signup', to: 'users#create'
+
+  resources :users, only: [:new,:create]
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  #delete 'logout', to: 'sessions#destroy'
+  get 'logout', to: 'sessions#destroy'
+
+  root "articles#index"
 
   get "up" => "rails/health#show", as: :rails_health_check
 
