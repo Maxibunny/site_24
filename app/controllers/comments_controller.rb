d66ad471ce6c65716d00beb1ec45fb0d72beb0f6
Maxibunny <max2003rnd@gmail.com>
2024-06-02 02:12:56 +0300

    update comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,7 @@
 class CommentsController < ApplicationController
 
   before_action :require_user
+  before_action :require_owner, only: [:destroy]
 
   def create
     @article = Article.find(params[:article_id])
@@ -8,12 +9,35 @@ class CommentsController < ApplicationController
     redirect_to article_path(@article)
   end
 
+  def require_owner
+    @comment = Comment.find(params[:id])
+    unless @comment.user_id == current_user.id
+      redirect_to root_path, alert: "You are not authorized to delete this comment"
+    end
+  end
+
+  def update
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.find(params[:id])
+
+    if @comment.update(comment_params)
+      @comment.update(updated_at: Time.now)
+      redirect_to article_path(@article)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
 
   def destroy
     @article = Article.find(params[:article_id])
     @comment = @article.comments.find(params[:id])
-    @comment.destroy
-    redirect_to article_path(@article), status: :see_other
+
+    if @comment.user_id == current_user.id
+      @comment.destroy
+      redirect_to article_path(@article), status: :see_other
+    else
+      redirect_to root_path, alert: "You are not authorized to delete this comment"
+    end
   end
 
   private
