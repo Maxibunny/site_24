c69505ac848c8e457f584e8013d02aee8029609c
yssv-vw <118190708+yssv-vw@users.noreply.github.com>
2024-06-04 16:43:02 +0300

    comment_test.rb

--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class CommentTest < ActiveSupport::TestCase
   test "Comment can't be without body" do
-    com = Comment.new(commenter: "Sasha")
+    com = Comment.new(commenter: "example@test.com")
     assert_not com.save, "Comment can't be empty"
   end
 end
