d97f72b832cfa6d269b4ca963bf8b5853d6728f4
DaniilKubantsev <daniil.kubantsev@gmail.com>
2024-06-05 12:24:37 +0300

    dfghdf

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ApplicationRecord
   has_secure_password
 
+  has_many :articles, dependent: :destroy
+  has_many :comments
+
   validates :email, presence: true, uniqueness: true, format: {with: URI::MailTo::EMAIL_REGEXP}
   validates :password, presence: true, length: { minimum: 5 }, confirmation: true
 
-  has_many :articles
-  has_many :comments
 end
